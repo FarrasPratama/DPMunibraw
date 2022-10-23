74b75e341a704fa27852db4b928046c6bc34aa57
FarrasPratama <pratama007fr@gmail.com>
2022-10-23 14:50:29 +0700

    Add BPI In Struktural

--- struktural.php
+++ struktural.php
@@ -10,7 +10,7 @@
             <div class="col-md-12">
                 <div class="titlepage">
                     <center>
-                        <h2 class="bawahtengah text-center">Struktur Keanggotaan DPM Unibraw</h2>
+                        <h2 class="bawahtengah text-center">BPI</h2>
                     </center>
                 </div>
             </div>
@@ -20,11 +20,11 @@
                 <div class="position-relative rounded overflow-hidden">
                     <div class="bigger">
                         <div class="overflow-hidden">
-                            <img class="img-fluid" src="assets/profil.jpg" alt="" width="100%" style="object-fit:cover;height:400px;">
+                            <img class="img-fluid" src="BPI/Fachrozi Reza_FISIP_2019_Ketua DPM UB/Fachrozi Reza_Ketua Umum_FISIP 2019.png" alt="" width="100%" style="object-fit:cover;height:400px;">
                         </div>
                         <div class="smaller">
-                            <h3 class="text-center text-white">John Doe</h3>
-                            <h4 class="text-center tekskuning">Jabatan</h4>
+                            <h3 class="text-center text-white">Fachrozi Reza</h3>
+                            <p class="text-center tekskuning">Ketua Umum<br>FISIP 2019</p>
                         </div>
                     </div>
                 </div>
@@ -33,11 +33,11 @@
                 <div class="position-relative rounded overflow-hidden">
                     <div class="bigger">
                         <div class="overflow-hidden">
-                            <img class="img-fluid" src="assets/profil.jpg" alt="" width="100%" style="object-fit:cover;height:400px;">
+                            <img class="img-fluid" src="BPI/Rafif Ananda Gusti_FH_2019_SEKJEND/IMG_3153-.png" alt="" width="100%" style="object-fit:cover;height:400px;">
                         </div>
                         <div class="smaller">
-                            <h3 class="text-center text-white">John Doe</h3>
-                            <h4 class="text-center tekskuning">Jabatan</h4>
+                            <h3 class="text-center text-white">Rafif Ananda Gusti</h3>
+                            <p class="text-center tekskuning">Sekretaris Jenderal<br>FH 2019</p>
                         </div>
                     </div>
                 </div>
@@ -46,11 +46,11 @@
                 <div class="position-relative rounded overflow-hidden">
                     <div class="bigger">
                         <div class="overflow-hidden">
-                            <img class="img-fluid" src="assets/profil.jpg" alt="" width="100%" style="object-fit:cover;height:400px;">
+                            <img class="img-fluid" src="BPI/Ahmad wafinindom_FIB_2019_WASEKJEND/IMG_3128.jpg" alt="" width="100%" style="object-fit:cover;height:400px;">
                         </div>
                         <div class="smaller">
-                            <h3 class="text-center text-white">John Doe</h3>
-                            <h4 class="text-center tekskuning">Jabatan</h4>
+                            <h3 class="text-center text-white">Ahmad Wafinindom</h3>
+                            <p class="text-center tekskuning">Wakil Sekretaris Jenderal<br>FIB 2019</p>
                         </div>
                     </div>
                 </div>
@@ -59,13 +59,22 @@
                 <div class="position-relative rounded overflow-hidden">
                     <div class="bigger">
                         <div class="overflow-hidden">
-                            <img class="img-fluid" src="assets/profil.jpg" alt="" width="100%" style="object-fit:cover;height:400px;">
+                            <img class="img-fluid" src="BPI/SELVIA MEGA ANGGRAINI_BENDUM/Selvia Mega Anggraini_Bendahara Umum_Vokasi 2019.png" alt="" width="100%" style="object-fit:cover;height:400px;">
                         </div>
                         <div class="smaller">
-                            <h3 class="text-center text-white">John Doe</h3>
-                            <h4 class="text-center tekskuning">Jabatan</h4>
+                            <h3 class="text-center text-white">Selvia Mega Anggraini</h3>
+                            <p class="text-center tekskuning">Bendahara Umum<br>Vokasi 2020</p>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col-md-12">
+                <div class="titlepage">
+                    <center>
+                        <h2 class="bawahtengah text-center">Struktur Keanggotaan DPM Unibraw</h2>
+                    </center>
                 </div>
             </div>
         </div>
