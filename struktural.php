390f439e59600dc3a2ffd5ec15a917e082f4ec76
FarrasPratama <pratama007fr@gmail.com>
2022-10-23 15:36:49 +0700

    Add BPH In Struktural

--- struktural.php
+++ struktural.php
@@ -69,12 +69,224 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div class="container">
         <div class="row mb-3">
             <div class="col-md-12">
                 <div class="titlepage">
                     <center>
-                        <h2 class="bawahtengah text-center">Struktur Keanggotaan DPM Unibraw</h2>
+                        <h2 class="bawahtengah text-center">BPH</h2>
                     </center>
+                </div>
+            </div>
+        </div>
+        <div class="row g-4 mb-3 mt-5">
+            <div class="col-md-3 col-xs-6 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="position-relative rounded overflow-hidden">
+                    <div class="bigger">
+                        <div class="overflow-hidden">
+                            <img class="img-fluid" src="BPH/BADAN ADVOKASI/R.M Agung Y._FIA_2019_BADVO/IMG_4066.HEIC" alt="" width="100%" style="object-fit:cover;height:400px;">
+                        </div>
+                        <div class="smaller">
+                            <h3 class="text-center text-white">R.M Agung Y.</h3>
+                            <p class="text-center tekskuning">Badan Advokasi<br>FIA 2019</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="position-relative rounded overflow-hidden">
+                    <div class="bigger">
+                        <div class="overflow-hidden">
+                            <img class="img-fluid" src="BPH/BADAN HUMAS DAN KELEMBAGAAN/Chandra Pramana Aldiputra_FP_Bahum/IMG_9161.HEIC" alt="" width="100%" style="object-fit:cover;height:400px;">
+                        </div>
+                        <div class="smaller">
+                            <h3 class="text-center text-white">Chandra Pramana Aldiputra</h3>
+                            <p class="text-center tekskuning">Badan Humas dan Kelembagaan<br>FP 2019</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="position-relative rounded overflow-hidden">
+                    <div class="bigger">
+                        <div class="overflow-hidden">
+                            <img class="img-fluid" src="BPH/BADAN HUMAS DAN KELEMBAGAAN/Salsa Al Fitri N.M_FKG_2019_Anggota Bahum/Salsa 1.png" alt="" width="100%" style="object-fit:cover;height:400px;">
+                        </div>
+                        <div class="smaller">
+                            <h3 class="text-center text-white">Salsa Al Fitri</h3>
+                            <p class="text-center tekskuning">Badan Humas dan Kelembagaan<br>FKG 2019</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="position-relative rounded overflow-hidden">
+                    <div class="bigger">
+                        <div class="overflow-hidden">
+                            <img class="img-fluid" src="BPH/BADAN LEGISLASI/Ishom Avenzour Ardjawinata/IMG_9175.HEIC" alt="" width="100%" style="object-fit:cover;height:400px;">
+                        </div>
+                        <div class="smaller">
+                            <h3 class="text-center text-white">Ishom Avenzour A.</h3>
+                            <p class="text-center tekskuning">Badan Legislasi<br>FTP 2019</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="position-relative rounded overflow-hidden">
+                    <div class="bigger">
+                        <div class="overflow-hidden">
+                            <img class="img-fluid" src="BPH/BADAN LEGISLASI/Muhammad Ivan Wiryawan_FILKOM_2019_Anggota Badan Legislatif/1_Transparent.png" alt="" width="100%" style="object-fit:cover;height:400px;">
+                        </div>
+                        <div class="smaller">
+                            <h3 class="text-center text-white">Ivan Wiryawan</h3>
+                            <p class="text-center tekskuning">Badan Legislasi<br>FILKOM 2019</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="position-relative rounded overflow-hidden">
+                    <div class="bigger">
+                        <div class="overflow-hidden">
+                            <img class="img-fluid" src="BPH/KOMINFO/Fatia NF/1.png" alt="" width="100%" style="object-fit:cover;height:400px;">
+                        </div>
+                        <div class="smaller">
+                            <h3 class="text-center text-white">Fatia N. F.</h3>
+                            <p class="text-center tekskuning">Kominfo<br>n/a</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="position-relative rounded overflow-hidden">
+                    <div class="bigger">
+                        <div class="overflow-hidden">
+                            <img class="img-fluid" src="BPH/KOMINFO/Indira Putri Maharani_FH_2020/2.png" alt="" width="100%" style="object-fit:cover;height:400px;">
+                        </div>
+                        <div class="smaller">
+                            <h3 class="text-center text-white">Indira Putri M.</h3>
+                            <p class="text-center tekskuning">Kominfo<br>FH 2020</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="position-relative rounded overflow-hidden">
+                    <div class="bigger">
+                        <div class="overflow-hidden">
+                            <img class="img-fluid" src="BPH/KOMISI 1/Hotijeh_Teknik_19_Ketua Komisi 1/Tije 1_Rev.jpg" alt="" width="100%" style="object-fit:cover;height:400px;">
+                        </div>
+                        <div class="smaller">
+                            <h3 class="text-center text-white">Hotijeh</h3>
+                            <p class="text-center tekskuning">Ketua Komisi 1<br>n/a</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="position-relative rounded overflow-hidden">
+                    <div class="bigger">
+                        <div class="overflow-hidden">
+                            <img class="img-fluid" src="BPH/KOMISI 1/Nona Maharani Al Fariza_FIKES_2020_Anggota Komisi 1/Nona 1_20220219_174839.jpg" alt="" width="100%" style="object-fit:cover;height:400px;">
+                        </div>
+                        <div class="smaller">
+                            <h3 class="text-center text-white">Nona Maharani A. F.</h3>
+                            <p class="text-center tekskuning">Anggota Komisi 1<br>FIKES 2020</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="position-relative rounded overflow-hidden">
+                    <div class="bigger">
+                        <div class="overflow-hidden">
+                            <img class="img-fluid" src="BPH/KOMISI 2/Muhammad Wafa Zidniilman_Ketua Komisi 2/S__30367749.jpg" alt="" width="100%" style="object-fit:cover;height:400px;">
+                        </div>
+                        <div class="smaller">
+                            <h3 class="text-center text-white">Muhammad Wafa Zidniilman</h3>
+                            <p class="text-center tekskuning">Ketua Komisi 2<br>n/a</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="position-relative rounded overflow-hidden">
+                    <div class="bigger">
+                        <div class="overflow-hidden">
+                            <img class="img-fluid" src="BPH/KOMISI 2/ABDUL HUSYAIN_FAPET_2020_ANGGOTA KOMISI/IMG_5901.JPG" alt="" width="100%" style="object-fit:cover;height:400px;">
+                        </div>
+                        <div class="smaller">
+                            <h3 class="text-center text-white">Abdul Husyain</h3>
+                            <p class="text-center tekskuning">Anggota Komisi 2<br>FAPET 2020</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="position-relative rounded overflow-hidden">
+                    <div class="bigger">
+                        <div class="overflow-hidden">
+                            <img class="img-fluid" src="BPH/KOMISI 3/Ali Zainal Abidin_FPIK_2019_Ketua Komisi 3/Ali Zainal Abidin_FPIK_2019_Ketua Komisi 3(2).jpg" alt="" width="100%" style="object-fit:cover;height:400px;">
+                        </div>
+                        <div class="smaller">
+                            <h3 class="text-center text-white">Ali Zainal</h3>
+                            <p class="text-center tekskuning">Ketua Komisi 3<br>FPIK 2019</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="position-relative rounded overflow-hidden">
+                    <div class="bigger">
+                        <div class="overflow-hidden">
+                            <img class="img-fluid" src="BPH/KOMISI 3/Zahra Jamilah Sabrina_FKH_2019_Anggota/1.png" alt="" width="100%" style="object-fit:cover;height:400px;">
+                        </div>
+                        <div class="smaller">
+                            <h3 class="text-center text-white">Zahra Jamilah S.</h3>
+                            <p class="text-center tekskuning">Anggota Komisi 3<br>FKH 2019</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="position-relative rounded overflow-hidden">
+                    <div class="bigger">
+                        <div class="overflow-hidden">
+                            <img class="img-fluid" src="BPH/KOMISI 4/Risa umami_Fmipa_2020_Ketua komisi  4/IMG_20220220_145859.jpg" alt="" width="100%" style="object-fit:cover;height:400px;">
+                        </div>
+                        <div class="smaller">
+                            <h3 class="text-center text-white">Risa Umami</h3>
+                            <p class="text-center tekskuning">Ketua Komisi 4<br>FMIPA 2020</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="position-relative rounded overflow-hidden">
+                    <div class="bigger">
+                        <div class="overflow-hidden">
+                            <img class="img-fluid" src="BPH/KOMISI 4/Maura Maharani Putri_FK_2020_Anggota Komisi 4/Mora 1.jpg" alt="" width="100%" style="object-fit:cover;height:400px;">
+                        </div>
+                        <div class="smaller">
+                            <h3 class="text-center text-white">Maura Maharani P.</h3>
+                            <p class="text-center tekskuning">Anggota Komisi 4<br>FK 2020</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xs-6 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="position-relative rounded overflow-hidden">
+                    <div class="bigger">
+                        <div class="overflow-hidden">
+                            <img class="img-fluid" src="BPH/KOMISI 5/Ketua  Komisi 5_Anugerah Cendekia Labaika Panjaitan/1.png" alt="" width="100%" style="object-fit:cover;height:400px;">
+                        </div>
+                        <div class="smaller">
+                            <h3 class="text-center text-white">Anugerah Cendekia L. P.</h3>
+                            <p class="text-center tekskuning">Ketua Komisi 5<br>n/a</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
